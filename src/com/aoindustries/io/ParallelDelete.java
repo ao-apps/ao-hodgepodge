1ab91cd2caf5e0a3e9fbb4d4c248899984aa3567
Dan Armstrong <dan@aoindustries.com>
2009-02-10 02:27:14 +0000

    Decoupled deletes into separate thread.

--- src/com/aoindustries/io/ParallelDelete.java
+++ src/com/aoindustries/io/ParallelDelete.java
@@ -28,29 +28,24 @@ import java.util.concurrent.BlockingQueue;
  * 
  * TODO: Verify this is, in fact, true.
  * 
- * This is measured with a copy of the backups from www1.fc.showsandshoots.com.
+ * This is measured with a copy of the backups from one of our managed servers.
  *
  *             +---------------------+---------------------+
  *             |         ext3        |      reiserfs       |
  * +-----------+----------+----------+----------+----------+
  * | # Deleted | parallel |  rm -rf  | parallel |  rm -rf  |
  * +-----------+----------+----------+----------+----------+
- * |      1/13 |          |          |          |          |
- * |      2/13 |          |          |          |          |
- * |      3/13 |          |          |          |          |
- * |      4/13 |          |          |          |          |
- * |      8/13 |          |          |          |          |
- * |     13/13 |          |          |          |          |
+ * |      1/13 |     TODO |     TODO |     TODO |     TODO |
+ * |      2/13 |     TODO |     TODO |     TODO |     TODO |
+ * |      3/13 |     TODO |     TODO |     TODO |     TODO |
+ * |      4/13 |     TODO |     TODO |     TODO |     TODO |
+ * |      8/13 |     TODO |     TODO |     TODO |     TODO |
+ * |     13/13 |     TODO |     TODO |     TODO |     TODO |
  * +-----------+----------+----------+----------+----------+
  * 
  * TODO: Use this from the back-up clean-up code if it is, in fact, faster.
  * 
- * TODO: Would there be any benefit to throughput by putting delete requests into
- *       a separate thread so the reads may occur concurrently with the deletes?
- *       On this note, concurrent deletes are also possible, but would it help?
- * 
- * TODO: It would also be possible to buffer all verbose output to verboseLog in
- *       a similar fashion to have logging affect the throughput less.
+ * TODO: Concurrent deletes would be possible.  Is there any advantage?
  *
  * @author  AO Industries, Inc.
  */
@@ -59,7 +54,7 @@ public class ParallelDelete {
     /**
      * The size of the delete queue.
      */
-    //private static final int DELETE_QUEUE_SIZE = 1000;
+    private static final int DELETE_QUEUE_SIZE = 1000;
 
     /**
      * The size of the verbose output queue.
@@ -104,7 +99,40 @@ public class ParallelDelete {
      * trees.
      */
     public static void parallelDelete(List<File> directories, final PrintStream verboseOutput) throws IOException {
-        final BlockingQueue<String> verboseQueue;
+        // The set of next files is kept in key order so that it can scale with O(n*log(n)) for larger numbers of directories
+        // as opposed to O(n^2) for a list.  This is similar to the fix for AWStats logresolvemerge provided by Dan Armstrong
+        // a couple of years ago.
+        final Map<String,SortedSet<FilesystemIterator>> nextFiles = new TreeMap<String,SortedSet<FilesystemIterator>>(
+            new Comparator<String>() {
+                public int compare(String S1, String S2) {
+                    // Make sure directories are sorted after their directory contents
+                    int diff = S1.compareTo(S2);
+                    if(diff==0) return 0;
+                    if(S2.startsWith(S1)) return 1;
+                    if(S1.startsWith(S2)) return -1;
+                    return diff;
+                }
+            }
+        );
+        {
+            final Map<String,FilesystemIteratorRule> prefixRules = Collections.emptyMap();
+            for(File directory : directories) {
+                if(!directory.exists()) throw new IOException("Directory not found: "+directory.getPath());
+                if(!directory.isDirectory()) throw new IOException("Not a directory: "+directory.getPath());
+                String path = directory.getCanonicalPath();
+                Map<String,FilesystemIteratorRule> rules = Collections.singletonMap(path, FilesystemIteratorRule.OK);
+                FilesystemIterator iterator = new FilesystemIterator(rules, prefixRules, path, false);
+                File nextFile = iterator.getNextFile();
+                if(nextFile!=null) {
+                    String relPath = getRelativePath(nextFile, iterator);
+                    SortedSet<FilesystemIterator> list = nextFiles.get(relPath);
+                    if(list==null) nextFiles.put(relPath, list = new TreeSet<FilesystemIterator>());
+                    list.add(iterator);
+                }
+            }
+        }
+
+        final BlockingQueue<File> verboseQueue;
         final boolean[] verboseThreadRun;
         Thread verboseThread;
         if(verboseOutput==null) {
@@ -112,9 +140,9 @@ public class ParallelDelete {
             verboseThreadRun = null;
             verboseThread = null;
         } else {
-            verboseQueue = new ArrayBlockingQueue<String>(VERBOSE_QUEUE_SIZE);
+            verboseQueue = new ArrayBlockingQueue<File>(VERBOSE_QUEUE_SIZE);
             verboseThreadRun = new boolean[] {true};
-            verboseThread = new Thread() {
+            verboseThread = new Thread("ParallelDelete - Verbose Thread") {
                 @Override
                 public void run() {
                     while(true) {
@@ -122,7 +150,7 @@ public class ParallelDelete {
                             if(!verboseThreadRun[0] && verboseQueue.isEmpty()) break;
                         }
                         try {
-                            verboseOutput.println(verboseQueue.take());
+                            verboseOutput.println(verboseQueue.take().getPath());
                             if(verboseQueue.isEmpty()) verboseOutput.flush();
                         } catch(InterruptedException err) {
                             // Normal during thread shutdown
@@ -133,73 +161,101 @@ public class ParallelDelete {
             verboseThread.start();
         }
         try {
-            // The set of next files is kept in key order so that it can scale with O(n*log(n)) for larger numbers of directories
-            // as opposed to O(n^2) for a list.  This is similar to the fix for AWStats logresolvemerge provided by Dan Armstrong
-            // a couple of years ago.
-            Map<String,SortedSet<FilesystemIterator>> nextFiles = new TreeMap<String,SortedSet<FilesystemIterator>>(
-                new Comparator<String>() {
-                    public int compare(String S1, String S2) {
-                        // Make sure directories are sorted after their directory contents
-                        int diff = S1.compareTo(S2);
-                        if(diff==0) return 0;
-                        if(S2.startsWith(S1)) return 1;
-                        if(S1.startsWith(S2)) return -1;
-                        return diff;
-                    }
-                }
-            );
-            {
-                final Map<String,FilesystemIteratorRule> prefixRules = Collections.emptyMap();
-                for(File directory : directories) {
-                    if(!directory.exists()) throw new IOException("Directory not found: "+directory.getPath());
-                    if(!directory.isDirectory()) throw new IOException("Not a directory: "+directory.getPath());
-                    String path = directory.getCanonicalPath();
-                    Map<String,FilesystemIteratorRule> rules = Collections.singletonMap(path, FilesystemIteratorRule.OK);
-                    FilesystemIterator iterator = new FilesystemIterator(rules, prefixRules, path, false);
-                    File nextFile = iterator.getNextFile();
-                    if(nextFile!=null) {
-                        String relPath = getRelativePath(nextFile, iterator);
-                        SortedSet<FilesystemIterator> list = nextFiles.get(relPath);
-                        if(list==null) nextFiles.put(relPath, list = new TreeSet<FilesystemIterator>());
-                        list.add(iterator);
-                    }
-                }
-            }
-
-            // Main loop, continue until nextFiles is empty
-            StringBuilder SB = new StringBuilder();
-            while(true) {
-                Iterator<String> iter = nextFiles.keySet().iterator();
-                if(!iter.hasNext()) break;
-                String relPath = iter.next();
-                for(FilesystemIterator iterator : nextFiles.remove(relPath)) {
-                    SB.setLength(0);
-                    SB.append(iterator.getStartPath());
-                    SB.append(relPath);
-                    String fullPath = SB.toString();
-                    if(verboseQueue!=null) {
+            final BlockingQueue<File> deleteQueue = new ArrayBlockingQueue<File>(DELETE_QUEUE_SIZE);
+            final boolean[] deleteThreadRun = new boolean[] {true};
+            final IOException[] deleteException = new IOException[1];
+            Thread deleteThread = new Thread("ParallelDelete - Delete Thread") {
+                @Override
+                public void run() {
+                    while(true) {
+                        synchronized(deleteThreadRun) {
+                            if(!deleteThreadRun[0] && deleteQueue.isEmpty()) break;
+                        }
                         try {
-                            verboseQueue.put(fullPath);
+                            File deleteme = deleteQueue.take();
+                            boolean doDelete;
+                            synchronized(deleteException) {
+                                doDelete = deleteException[0]==null;
+                            }
+                            if(doDelete) {
+                                try {
+                                    if(verboseQueue!=null) {
+                                        boolean done = false;
+                                        while(!done) {
+                                            try {
+                                                verboseQueue.put(deleteme);
+                                                done = true;
+                                            } catch(InterruptedException err) {
+                                                // Normal during thread shutdown
+                                            }
+                                        }
+                                    }
+                                    if(!deleteme.delete()) throw new IOException("Unable to delete: "+deleteme.getPath());
+                                } catch(IOException err) {
+                                    synchronized(deleteException) {
+                                        deleteException[0] = err;
+                                    }
+                                }
+                            }
+                        } catch(InterruptedException err) {
+                            // Normal during thread shutdown
+                        }
+                    }
+                }
+            };
+            deleteThread.start();
+            try {
+                // Main loop, continue until nextFiles is empty
+                final StringBuilder SB = new StringBuilder();
+                while(true) {
+                    synchronized(deleteException) {
+                        if(deleteException[0]!=null) break;
+                    }
+                    Iterator<String> iter = nextFiles.keySet().iterator();
+                    if(!iter.hasNext()) break;
+                    String relPath = iter.next();
+                    for(FilesystemIterator iterator : nextFiles.remove(relPath)) {
+                        SB.setLength(0);
+                        SB.append(iterator.getStartPath());
+                        SB.append(relPath);
+                        String fullPath = SB.toString();
+                        try {
+                            deleteQueue.put(new File(fullPath));
                         } catch(InterruptedException err) {
                             IOException ioErr = new InterruptedIOException();
                             ioErr.initCause(err);
                             throw ioErr;
                         }
+                        // Get the next file
+                        File nextFile = iterator.getNextFile();
+                        if(nextFile!=null) {
+                            String newRelPath = getRelativePath(nextFile, iterator);
+                            SortedSet<FilesystemIterator> list = nextFiles.get(newRelPath);
+                            if(list==null) nextFiles.put(newRelPath, list = new TreeSet<FilesystemIterator>());
+                            list.add(iterator);
+                        }
                     }
-                    File deleteme = new File(fullPath);
-                    if(!deleteme.delete()) throw new IOException("Unable to delete: "+fullPath);
-                    // Get the next file
-                    File nextFile = iterator.getNextFile();
-                    if(nextFile!=null) {
-                        String newRelPath = getRelativePath(nextFile, iterator);
-                        SortedSet<FilesystemIterator> list = nextFiles.get(newRelPath);
-                        if(list==null) nextFiles.put(newRelPath, list = new TreeSet<FilesystemIterator>());
-                        list.add(iterator);
-                    }
+                }
+            } finally {
+                // Wait for delete queue to be empty
+                synchronized(deleteThreadRun) {
+                    deleteThreadRun[0] = false;
+                }
+                deleteThread.interrupt();
+                try {
+                    deleteThread.join();
+                } catch(InterruptedException err) {
+                    IOException ioErr = new InterruptedIOException();
+                    ioErr.initCause(err);
+                    throw ioErr;
+                }
+                // Throw any exception that caused this to stop
+                synchronized(deleteException) {
+                    if(deleteException[0]!=null) throw deleteException[0];
                 }
             }
         } finally {
-            // Wait for queue to be empty
+            // Wait for verbose queue to be empty
             if(verboseThread!=null) {
                 synchronized(verboseThreadRun) {
                     verboseThreadRun[0] = false;
