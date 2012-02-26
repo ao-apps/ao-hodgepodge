f3e3e6785e877a716b1e40402b7916b6c0b39bdb
Dan Armstrong <dan@aoindustries.com>
2012-02-26 10:30:22 +0000

    New exception type for code that requires login.

---
+++ src/com/aoindustries/security/NotLoggedInException.java
@@ -0,0 +1,50 @@
+/*
+ * aocode-public - Reusable Java library of general tools with minimal external dependencies.
+ * Copyright (C) 2012  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aocode-public.
+ *
+ * aocode-public is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aocode-public is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aocode-public.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.security;
+
+/**
+ * Thrown when code that requires authentication is invoked in a context
+ * that doesn't have an authenticated user.
+ *
+ * @author  AO Industries, Inc.
+ */
+public class NotLoggedInException extends SecurityException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NotLoggedInException() {
+        super();
+    }
+    
+    public NotLoggedInException(String message) {
+        super(message);
+    }
+
+    public NotLoggedInException(Throwable cause) {
+        super(cause);
+    }
+    
+    public NotLoggedInException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
