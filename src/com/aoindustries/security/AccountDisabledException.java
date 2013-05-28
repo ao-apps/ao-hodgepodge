4e992b24938d76c390a08a65f34020f30c88ec43
Dan Armstrong <dan@aoindustries.com>
2013-05-28 02:43:43 +0000

    Moving to head

--- src/com/aoindustries/security/AccountDisabledException.java
+++ src/com/aoindustries/security/AccountDisabledException.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011  AO Industries, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -32,7 +32,7 @@ public class AccountDisabledException extends LoginException {
     public AccountDisabledException() {
         super();
     }
-    
+
     public AccountDisabledException(String message) {
         super(message);
     }
