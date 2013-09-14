3543375315ca3294ec28a3785f5ae393be614cd2
Dan Armstrong <dan@aoindustries.com>
2013-09-14 15:25:09 +0000

    Renamed to Charsets to avoid requiring full package name to disambiguate from standard Charset class.

--- src/com/aoindustries/nio/charset/Charsets.java
+++ src/com/aoindustries/nio/charset/Charsets.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2012  AO Industries, Inc.
+ * Copyright (C) 2012, 2013  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,25 +22,27 @@
  */
 package com.aoindustries.nio.charset;
 
+import java.nio.charset.Charset;
+
 /**
  * Defines some constants for handling standard character sets.
  *
  * @author  AO Industries, Inc.
  */
-public class Charset {
+public class Charsets {
 
-    public static final java.nio.charset.Charset
-        US_ASCII   = java.nio.charset.Charset.forName("US-ASCII"),
-        ISO_8859_1 = java.nio.charset.Charset.forName("ISO-8859-1"),
-        UTF_8      = java.nio.charset.Charset.forName("UTF-8"),
-        UTF_16BE   = java.nio.charset.Charset.forName("UTF-16BE"),
-        UTF_16LE   = java.nio.charset.Charset.forName("UTF-16LE"),
-        UTF_16     = java.nio.charset.Charset.forName("UTF-16")
+    public static final Charset
+        US_ASCII   = Charset.forName("US-ASCII"),
+        ISO_8859_1 = Charset.forName("ISO-8859-1"),
+        UTF_8      = Charset.forName("UTF-8"),
+        UTF_16BE   = Charset.forName("UTF-16BE"),
+        UTF_16LE   = Charset.forName("UTF-16LE"),
+        UTF_16     = Charset.forName("UTF-16")
     ;
 
     /**
      * Make no instances.
      */
-    private Charset() {
+    private Charsets() {
     }
 }
