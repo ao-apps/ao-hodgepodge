8cceabd70c932ce7c532ce4a3e8afe673ac506ec
Dan Armstrong <dan@aoindustries.com>
2014-05-08 06:39:55 +0000

    Working on Java implementation of both client and server for HTTP sockets.

--- src/com/aoindustries/messaging/AbstractSocketContext.java
+++ src/com/aoindustries/messaging/AbstractSocketContext.java
@@ -24,25 +24,37 @@ package com.aoindustries.messaging;
 
 import com.aoindustries.security.Identifier;
 import com.aoindustries.util.AoCollections;
+import com.aoindustries.util.concurrent.ExecutorService;
 import java.io.IOException;
 import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Base implementation of socket context.
  */
 abstract public class AbstractSocketContext<S extends Socket> implements SocketContext {
 
+	private static final Logger logger = Logger.getLogger(AbstractSocketContext.class.getName());
+
 	private final Map<Identifier,S> sockets = new LinkedHashMap<Identifier,S>();
 
+	/**
+	 * A queue of events per listener.  When the queue is null, no executors are running.
+	 * When the queue is non-null, even when empty, an executor is running.
+	 */
 	private final Map<SocketContextListener,Queue<Runnable>> listeners = new IdentityHashMap<SocketContextListener,Queue<Runnable>>();
 
 	private volatile boolean closed;
 
+	private final ExecutorService executor = ExecutorService.newInstance();
+
 	protected AbstractSocketContext() {
+		// On startup get the executor
 	}
 
 	@Override
@@ -62,6 +74,7 @@ abstract public class AbstractSocketContext<S extends Socket> implements SocketC
 			for(S socket : sockets.values()) socket.close();
 			sockets.clear();
 		}
+		executor.dispose();
 	}
 
 	@Override
@@ -73,14 +86,75 @@ abstract public class AbstractSocketContext<S extends Socket> implements SocketC
 	public void addSocketContextListener(SocketContextListener listener) throws IllegalStateException {
 		synchronized(listeners) {
 			if(listeners.containsKey(listener)) throw new IllegalStateException("listener already added");
-			listeners.put(listener, new LinkedList<Runnable>());
+			listeners.put(listener, null);
 		}
 	}
 
 	@Override
 	public boolean removeSocketContextListener(SocketContextListener listener) {
 		synchronized(listeners) {
-			return listeners.remove(listener) != null;
+			if(!listeners.containsKey(listener)) {
+				return false;
+			} else {
+				listeners.remove(listener);
+				return true;
+			}
+		}
+	}
+
+	/**
+	 * Enqueues calls to onNewSocket on all listeners.
+	 */
+	protected void enqueueOnNewSocket(final S newSocket) {
+		synchronized(listeners) {
+			for(Map.Entry<SocketContextListener,Queue<Runnable>> entry : listeners.entrySet()) {
+				final SocketContextListener listener = entry.getKey();
+				Queue<Runnable> queue = entry.getValue();
+				boolean isFirst = queue == null;
+				if(isFirst) {
+					queue = new LinkedList<Runnable>();
+					entry.setValue(queue);
+				}
+				queue.add(
+					new Runnable() {
+						@Override
+						public void run() {
+							listener.onNewSocket(AbstractSocketContext.this, newSocket);
+						}
+					}
+				);
+				if(isFirst) {
+					executor.submitUnbounded(
+						new Runnable() {
+							@Override
+							public void run() {
+								while(true) {
+									// Invoke each of the events until the queue is empty
+									Runnable event;
+									synchronized(listeners) {
+										Queue<Runnable> queue = listeners.get(listener);
+										if(queue.isEmpty()) {
+											// Remove the empty queue so a new executor will be submitted on next event
+											listeners.remove(listener);
+											return;
+										} else {
+											event = queue.remove();
+										}
+									}
+									// Run the event without holding the listeners lock
+									try {
+										event.run();
+									} catch(ThreadDeath TD) {
+										throw TD;
+									} catch(Throwable t) {
+										logger.log(Level.SEVERE, null, t);
+									}
+								}
+							}
+						}
+					);
+				}
+			}
 		}
 	}
 }
