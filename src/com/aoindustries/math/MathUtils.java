6167aaa7e61f47cc9f4afd7b2743dbca72b5976b
Dan Armstrong <dan@aoindustries.com>
2014-08-16 05:48:53 +0000

    Now supporting node.js

---
+++ src/com/aoindustries/math/MathUtils.java
@@ -0,0 +1,55 @@
+/*
+ * aocode-public - Reusable Java library of general tools with minimal external dependencies.
+ * Copyright (C) 2014  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aocode-public.
+ *
+ * aocode-public is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aocode-public is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aocode-public.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.math;
+
+/**
+ * Math routines that check for overflow conditions.
+ *
+ * @author  AO Industries, Inc.
+ */
+public class MathUtils {
+
+    private MathUtils() {
+    }
+
+	/**
+	 * Adds two integers together while allowing null.
+	 * 
+	 * @return  the sum or null when both arguments are null
+	 */
+	public static Integer add(Integer i1, Integer i2) {
+		if(i1 == null) {
+			if(i2 == null) {
+				return null;
+			} else {
+				return i2;
+			}
+		} else {
+			if(i2 == null) {
+				return i1;
+			} else {
+				return i1 + i2;
+			}
+		}
+	}
+}
