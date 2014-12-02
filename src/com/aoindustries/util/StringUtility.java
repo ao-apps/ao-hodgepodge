a492cb518db49dbc8990d2a431d8e959ce92a8a8
Dan Armstrong <dan@aoindustries.com>
2014-12-02 01:40:54 +0000

    Deprecated method used again.

--- src/com/aoindustries/util/StringUtility.java
+++ src/com/aoindustries/util/StringUtility.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013  AO Industries, Inc.
+ * Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -1064,10 +1064,6 @@ public final class StringUtility {
         } while (string.length() > 0);
     }
 
-    /**
-     * @deprecated  This method is no longer used and will be removed.
-     */
-    @Deprecated
     public static String convertToHex(byte[] bytes) {
         if(bytes==null) return null;
         int len=bytes.length;
