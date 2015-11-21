400b0560ea247ea8edf9d44d361c09b13ef2e6ea
Dan Armstrong <dan@aoindustries.com>
2015-11-21 05:57:34 +0000

    Cleaned up for newer Java version.

--- src/com/aoindustries/io/FilesystemIteratorRule.java
+++ src/com/aoindustries/io/FilesystemIteratorRule.java
@@ -28,36 +28,34 @@ import java.io.IOException;
  * Controls the behavior of a <code>FilesystemIterator</code>.  Each rule
  * has is mapped in via either a filename or a file prefix.  If a rule is provided
  * with an empty filename for regular rules (not prefix-matched), it will be the
- * default for all filesystem roots.
+ * default for all file system roots.
  *
  * @author  AO Industries, Inc.
  */
 abstract public class FilesystemIteratorRule {
 
-    /**
-     * Gets if this item should be included.
-     */
-    abstract public boolean isIncluded(String filename) throws IOException;
-    
-    /**
-     * A rule that will always be backed-up.
-     */
-    public static final FilesystemIteratorRule OK = new FilesystemIteratorRule() {
+	/**
+	 * Gets if this item should be included.
+	 */
+	abstract public boolean isIncluded(String filename) throws IOException;
 
+	/**
+	 * A rule that will always be backed-up.
+	 */
+	public static final FilesystemIteratorRule OK = new FilesystemIteratorRule() {
 		@Override
-        public boolean isIncluded(String filename) {
-            return true;
-        }
-    };
+		public boolean isIncluded(String filename) {
+			return true;
+		}
+	};
 
-    /**
-     * A rule that will not be backed-up.
-     */
-    public static final FilesystemIteratorRule SKIP = new FilesystemIteratorRule() {
-
+	/**
+	 * A rule that will not be backed-up.
+	 */
+	public static final FilesystemIteratorRule SKIP = new FilesystemIteratorRule() {
 		@Override
-        public boolean isIncluded(String filename) {
-            return false;
-        }
-    };
+		public boolean isIncluded(String filename) {
+			return false;
+		}
+	};
 }
