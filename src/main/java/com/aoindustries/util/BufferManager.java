a7b9431c3da2fa25440e8dde814246c35acd1525
AO Industries, Inc. <support@aoindustries.com>
2017-03-10 16:48:53 -0600

    BufferManager improvements to limit potential memory leak.

--- src/main/java/com/aoindustries/util/BufferManager.java
+++ src/main/java/com/aoindustries/util/BufferManager.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2016  AO Industries, Inc.
+ * Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2016, 2017  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -29,9 +29,9 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * <p>
- * <code>BufferManager</code> manages a reusable pool of <code>byte[]</code> and <code>char[]</code> buffers.
+ * {@code BufferManager} manages a reusable pool of {@code byte[]} and {@code char[]} buffers.
  * This avoids the repetitive allocation of memory for an operation that only needs a temporary buffer.
- * The buffers are stored as <code>ThreadLocal</code> to maximize cache locality.
+ * The buffers are stored as {@code ThreadLocal} to maximize cache locality.
  * </p>
  * <p>
  * Do not use if intra-thread security is more important than performance.
@@ -42,11 +42,23 @@ import java.util.concurrent.atomic.AtomicLong;
  * However, being thread local there remains some control over the visibility of the data.
  * </p>
  * <p>
- * Buffers must not be passed between threads, as each thread has an unlimited number of possible buffers.
- * Giving a thread a buffer you didn't get from it could result in a memory leak.
+ * Buffers should not be passed between threads.
+ * Giving a thread a buffer you didn't get from it could result in a memory or information leak.
+ * The number of buffers per thread is limited to avoid a complete runaway memory leak, but keeping
+ * buffers to a single thread is optimal.
+ * </p>
+ * <p>
+ * Under no circumstances should a buffer be released more than once.  This may result
+ * in the buffer being allocated twice at the same time, with resulting data corruption.
+ * </p>
+ * <p>
+ * The Java virtual machine has improved greatly over the years.  However, we still believe
+ * this buffer management to be valuable to reduce garbage collection pressure.  If this ever
+ * proves to not be the case, the implementation here can be simply changed to create new
+ * arrays on each use.
  * </p>
  *
- * TODO: Implement as concurrent queue/deque instead of thread locals?
+ * TODO: Java 1.7+: Implement as concurrent queue/deque instead of thread locals?
  *
  * @author  AO Industries, Inc.
  */
@@ -57,17 +69,22 @@ final public class BufferManager {
 	 */
 	public static final int BUFFER_SIZE = 4096;
 
+	/**
+	 * The maximum number of retained buffers per thread.
+	 */
+	private static final int MAX_BUFFERS_PER_THREAD = 32;
+
 	private static final ThreadLocal<Deque<byte[]>> bytes = new ThreadLocal<Deque<byte[]>>() {
 		@Override
 		public Deque<byte[]> initialValue() {
-			return new ArrayDeque<byte[]>();
+			return new ArrayDeque<byte[]>(MAX_BUFFERS_PER_THREAD);
 		}
 	};
 
 	private static final ThreadLocal<Deque<char[]>> chars = new ThreadLocal<Deque<char[]>>() {
 		@Override
 		public Deque<char[]> initialValue() {
-			return new ArrayDeque<char[]>();
+			return new ArrayDeque<char[]>(MAX_BUFFERS_PER_THREAD);
 		}
 	};
 
@@ -83,15 +100,19 @@ final public class BufferManager {
 	private static final AtomicLong
 		bytesCreates = new AtomicLong(),
 		bytesUses = new AtomicLong(),
+		bytesDiscards = new AtomicLong(),
+		bytesZeroFills = new AtomicLong(),
 		charsCreates = new AtomicLong(),
-		charsUses = new AtomicLong()
+		charsUses = new AtomicLong(),
+		charsDiscards = new AtomicLong(),
+		charsZeroFills = new AtomicLong()
 	;
 
 	/**
-	 * Gets a <code>byte[]</code> of length <code>BUFFER_SIZE</code> that may
+	 * Gets a {@code byte[]] of length {@code BUFFER_SIZE} that may
 	 * be temporarily used for any purpose.  Once done with the buffer,
-	 * <code>releaseBuffer</code> should be called, this is best accomplished
-	 * in a <code>finally</code> block.
+	 * {@code release} should be called, this is best accomplished
+	 * in a {@code finally} block.
 	 * The buffer is not necessarily zero-filled and may contain data from a previous use.
 	 */
 	public static byte[] getBytes() {
@@ -105,10 +126,10 @@ final public class BufferManager {
 	}
 
 	/**
-	 * Gets a <code>char[]</code> of length <code>BUFFER_SIZE</code> that may
+	 * Gets a {@code char[]} of length {@code BUFFER_SIZE} that may
 	 * be temporarily used for any purpose.  Once done with the buffer,
-	 * <code>releaseBuffer</code> should be called, this is best accomplished
-	 * in a <code>finally</code> block.
+	 * {@code release} should be called, this is best accomplished
+	 * in a {@code finally} block.
 	 * The buffer is not necessarily zero-filled and may contain data from a previous use.
 	 */
 	public static char[] getChars() {
@@ -130,18 +151,25 @@ final public class BufferManager {
 	}
 
 	/**
-	 * Releases a <code>byte[]</code> that was obtained by a call to
-	 * <code>getBytes</code>.
+	 * Releases a {@code byte[]} that was obtained by a call to
+	 * {@code getBytes}.  A buffer must not be released more than once.
 	 *
-	 * @param  buffer  the <code>byte[]</code> to release
+	 * @param  buffer  the {@code byte[]} to release
 	 * @param  zeroFill  if the data in the buffer may be sensitive, it is best to zero-fill the buffer on release.
 	 */
 	public static void release(byte[] buffer, boolean zeroFill) {
 		Deque<byte[]> myBytes = bytes.get();
 		if(buffer.length != BUFFER_SIZE) throw new IllegalArgumentException();
 		assert !inQueue(myBytes, buffer); // Error if already in the buffer list
-		if(zeroFill) Arrays.fill(buffer, 0, BUFFER_SIZE, (byte)0);
-		myBytes.add(buffer);
+		if(myBytes.size() >= MAX_BUFFERS_PER_THREAD) {
+			bytesDiscards.getAndIncrement();
+		} else {
+			if(zeroFill) {
+				bytesZeroFills.getAndIncrement();
+				Arrays.fill(buffer, 0, BUFFER_SIZE, (byte)0);
+			}
+			myBytes.add(buffer);
+		}
 	}
 	private static boolean inQueue(Iterable<byte[]> myBytes, byte[] buffer) {
 		for(byte[] inQueue : myBytes) if(inQueue==buffer) return true;
@@ -157,37 +185,84 @@ final public class BufferManager {
 	}
 
 	/**
-	 * Releases a <code>char[]</code> that was obtained by a call to
-	 * <code>getChars</code>.
+	 * Releases a {@code char[]} that was obtained by a call to
+	 * {@code getChars}.  A buffer must not be released more than once.
 	 *
-	 * @param  buffer  the <code>char[]</code> to release
+	 * @param  buffer  the {@code char[]} to release
 	 * @param  zeroFill  if the data in the buffer may be sensitive, it is best to zero-fill the buffer on release.
 	 */
 	public static void release(char[] buffer, boolean zeroFill) {
 		Deque<char[]> myChars = chars.get();
 		if(buffer.length != BUFFER_SIZE) throw new IllegalArgumentException();
 		assert !inQueue(myChars, buffer); // Error if already in the buffer list
-		if(zeroFill) Arrays.fill(buffer, 0, BUFFER_SIZE, (char)0);
-		myChars.add(buffer);
+		if(myChars.size() >= MAX_BUFFERS_PER_THREAD) {
+			charsDiscards.getAndIncrement();
+		} else {
+			if(zeroFill) {
+				charsZeroFills.getAndIncrement();
+				Arrays.fill(buffer, 0, BUFFER_SIZE, (char)0);
+			}
+			myChars.add(buffer);
+		}
 	}
 	private static boolean inQueue(Iterable<char[]> myChars, char[] buffer) {
 		for(char[] inQueue : myChars) if(inQueue == buffer) return true;
 		return false;
 	}
 
+	/**
+	 * Gets the number of {@code byte[]} buffers instantiated.
+	 */
 	public static long getByteBufferCreates() {
 		return bytesCreates.get();
 	}
 
+	/**
+	 * Gets the number of time {@code byte[]} buffers have been used.
+	 */
 	public static long getByteBufferUses() {
 		return bytesUses.get();
 	}
 
+	/**
+	 * Gets the number of time {@code byte[]} buffers have been discarded on release.
+	 */
+	public static long getByteBufferDiscards() {
+		return bytesDiscards.get();
+	}
+
+	/**
+	 * Gets the number of time {@code byte[]} buffers have been zero-filled on release.
+	 */
+	public static long getByteBufferZeroFills() {
+		return bytesZeroFills.get();
+	}
+
+	/**
+	 * Gets the number of {@code char[]} buffers instantiated.
+	 */
 	public static long getCharBufferCreates() {
 		return charsCreates.get();
 	}
 
+	/**
+	 * Gets the number of time {@code char[]} buffers have been used.
+	 */
 	public static long getCharBufferUses() {
 		return charsUses.get();
+	}
+
+	/**
+	 * Gets the number of time {@code char[]} buffers have been discarded on release.
+	 */
+	public static long getCharBufferDiscards() {
+		return charsDiscards.get();
+	}
+
+	/**
+	 * Gets the number of time {@code char[]} buffers have been zero-filled on release.
+	 */
+	public static long getCharBufferZeroFills() {
+		return charsZeroFills.get();
 	}
 }
