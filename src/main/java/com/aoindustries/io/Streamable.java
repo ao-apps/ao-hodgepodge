6858a5b2fd69e53be9ebf121eb71a790742c6ed6
AO Industries, Inc. <support@aoindustries.com>
2019-07-26 05:13:03 -0500

    Added protocol version to reading
    in Streamable and CompressedReadable, because a server may be reading
    objects streamed by older clients.  Protocol compatibility can be
    necessary both directions.

--- src/main/java/com/aoindustries/io/Streamable.java
+++ src/main/java/com/aoindustries/io/Streamable.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2016, 2017  AO Industries, Inc.
+ * Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2016, 2017, 2019  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -34,8 +34,8 @@ import java.io.IOException;
 public interface Streamable extends CompressedReadable, CompressedWritable {
 
 	@Override
-	void read(CompressedDataInputStream in) throws IOException;
+	void read(CompressedDataInputStream in, String protocolVersion) throws IOException;
 
 	@Override
-	void write(CompressedDataOutputStream out, String clientVersion) throws IOException;
+	void write(CompressedDataOutputStream out, String protocolVersion) throws IOException;
 }
