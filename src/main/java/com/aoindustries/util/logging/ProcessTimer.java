d0474597510c857cdb0b0d0969d726f8a96d7a8d
AO Industries, Inc. <support@aoindustries.com>
2019-07-29 04:40:09 -0500

    Removed unused import.

--- src/main/java/com/aoindustries/util/logging/ProcessTimer.java
+++ src/main/java/com/aoindustries/util/logging/ProcessTimer.java
@@ -23,7 +23,6 @@
 package com.aoindustries.util.logging;
 
 import com.aoindustries.util.StringUtility;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
