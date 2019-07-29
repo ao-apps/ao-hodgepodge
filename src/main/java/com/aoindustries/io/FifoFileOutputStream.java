14a5ad77d533f275bd3fe7bb5f4b6326070384fe
AO Industries, Inc. <support@aoindustries.com>
2019-07-29 01:27:16 -0500

    New method FifoFile.flush() that may be used to flush the fifo file.
    This makes sense here because reads also update the underlying fifo persistence.

--- src/main/java/com/aoindustries/io/FifoFileOutputStream.java
+++ src/main/java/com/aoindustries/io/FifoFileOutputStream.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2016, 2018  AO Industries, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2016, 2018, 2019  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -143,12 +143,12 @@ public class FifoFileOutputStream extends OutputStream {
 	/**
 	 * Flushes all updates to this file to the underlying storage device.  This is performed by
 	 * <code>RandomAccessFile.getChannel().force(true)</code>.
+	 *
+	 * @see  FifoFile#flush()
 	 */
 	@Override
 	public void flush() throws IOException {
-		synchronized(file) {
-			file.file.getChannel().force(true);
-		}
+		file.flush();
 	}
 
 	/**
