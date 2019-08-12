5704f6eab1de6e92ff776769896b8d199ad1281a
AO Industries, Inc. <support@aoindustries.com>
2019-08-12 07:43:10 -0500

    Fixed typo in comment.

--- src/main/java/com/aoindustries/net/HttpParameters.java
+++ src/main/java/com/aoindustries/net/HttpParameters.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2011, 2016  AO Industries, Inc.
+ * Copyright (C) 2011, 2016, 2019  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -40,7 +40,7 @@ public interface HttpParameters {
 	String getParameter(String name);
 
 	/**
-	 * Gets an unmodifiable interator of the parameter names.
+	 * Gets an unmodifiable iterator of the parameter names.
 	 */
 	Iterator<String> getParameterNames();
 
