8d575b186fc69f8c8c394f39db3e9a3c82d149b8
AO Industries, Inc. <support@aoindustries.com>
2019-08-18 01:14:30 -0500

    All implementations of HttpParameters.toString() now encode a query string in UTF-8.

--- src/main/java/com/aoindustries/net/HttpParametersUtils.java
+++ src/main/java/com/aoindustries/net/HttpParametersUtils.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2011, 2013, 2016  AO Industries, Inc.
+ * Copyright (C) 2011, 2013, 2016, 2019  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -23,52 +23,129 @@
 package com.aoindustries.net;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
 
 /**
- * Utilities using HttpParameters.
+ * Utilities using {@link HttpParameters}.
  *
  * @author  AO Industries, Inc.
  */
 final public class HttpParametersUtils {
 
 	/**
-	 * Adds all of the parameters to a URL.
+	 * Adds all of the parameters to a URI in a given encoding.
+	 *
+	 * @param documentEncoding  The name of a supported {@linkplain Charset character encoding}.
+	 *
+	 * @return  The new URI or {@code uri} when not modified
 	 */
-	public static String addParams(String href, HttpParameters params, String encoding) throws UnsupportedEncodingException {
-		if(params!=null) {
-			StringBuilder sb = new StringBuilder(href);
-			// First find any anchor and if has parameters
-			int anchorStart = href.lastIndexOf('#');
-			String anchor;
-			boolean hasQuestion;
-			if(anchorStart == -1) {
-				anchor = null;
-				hasQuestion = href.lastIndexOf('?') != -1;
-			} else {
-				anchor = href.substring(anchorStart);
-				sb.setLength(anchorStart);
-				hasQuestion = href.lastIndexOf('?', anchorStart-1) != -1;
-			}
-			for(Map.Entry<String,List<String>> entry : params.getParameterMap().entrySet()) {
-				String encodedName = URLEncoder.encode(entry.getKey(), encoding);
-				for(String value : entry.getValue()) {
-					if(hasQuestion) sb.append('&');
-					else {
-						sb.append('?');
+	public static String addParams(String uri, HttpParameters params, String documentEncoding) throws UnsupportedEncodingException {
+		if(params != null) {
+			Map<String, List<String>> paramsMap = params.getParameterMap();
+			int mapSize = paramsMap.size();
+			if(mapSize != 0) {
+				int uriLen = uri.length();
+				StringBuilder newUri = new StringBuilder(
+					uriLen
+					// 20 is arbitrary, just some default size scaled to number of parameters as a starting point
+					+ mapSize * 20
+				);
+				int anchorStart;
+				boolean hasQuestion;
+				{
+					// Find first of '?' or '#'
+					int pathEnd = UrlUtils.getPathEnd(uri);
+					if(pathEnd >= uriLen) {
+						// First parameter to end
+						newUri.append(uri);
+						anchorStart = -1;
+						hasQuestion = false;
+					} else if(uri.charAt(pathEnd) == '?') {
+						anchorStart = uri.indexOf('#', pathEnd + 1);
 						hasQuestion = true;
+						if(anchorStart == -1) {
+							// Additional parameter to end
+							newUri.append(uri);
+						} else {
+							// Additional parameter before anchor
+							newUri.append(uri, 0, anchorStart);
+						}
+					} else {
+						// First parameter before anchor
+						assert uri.charAt(pathEnd) == '#';
+						anchorStart = pathEnd;
+						newUri.append(uri, 0, anchorStart);
+						hasQuestion = false;
 					}
-					sb.append(encodedName);
-					assert value!=null : "null values no longer supported to be consistent with servlet environment";
-					sb.append('=').append(URLEncoder.encode(value, encoding));
 				}
+				for(Map.Entry<String,List<String>> entry : paramsMap.entrySet()) {
+					String name = entry.getKey();
+					String encodedName = UrlUtils.encodeURIComponent(name, documentEncoding);
+					for(String value : entry.getValue()) {
+						assert value != null : "null values no longer supported to be consistent with servlet environment";
+						if(hasQuestion) {
+							newUri.append('&');
+						} else {
+							newUri.append('?');
+							hasQuestion = true;
+						}
+						newUri.append(encodedName).append('=');
+						UrlUtils.encodeURIComponent(value, documentEncoding, newUri);
+					}
+				}
+				if(anchorStart != -1) newUri.append(uri, anchorStart, uriLen);
+				assert newUri.length() > uriLen : "Parameters must have been added";
+				uri = newUri.toString();
 			}
-			if(anchor!=null) sb.append(anchor);
-			href = sb.toString();
 		}
-		return href;
+		return uri;
+	}
+
+	/**
+	 * Gets the query string encoded in a given encoding,
+	 * not including the '?' prefix.
+	 *
+	 * @return  The query string or {@code null} for none.
+	 */
+	public static String toQueryString(HttpParameters params, String documentEncoding) throws UnsupportedEncodingException {
+		if(params == null) return null;
+		Map<String,List<String>> map = params.getParameterMap();
+		if(map.isEmpty()) return null;
+		StringBuilder sb = new StringBuilder();
+		boolean didOne = false;
+		for(Map.Entry<String,List<String>> entry : map.entrySet()) {
+			String encodedName = UriComponent.QUERY.encode(entry.getKey(), documentEncoding);
+			for(String value : entry.getValue()) {
+				if(didOne) {
+					sb.append('&');
+				} else {
+					didOne = true;
+				}
+				sb.append(encodedName);
+				assert value != null : "null values no longer supported to be consistent with servlet environment";
+				sb.append('=');
+				UriComponent.QUERY.encode(value, documentEncoding, sb);
+			}
+		}
+		assert didOne;
+		return sb.toString();
+	}
+
+	/**
+	 * Returns the optimal type of parameters.
+	 * <ol>
+	 * <li>When @{code queryString} is {@code null} or {@code ""}: {@link EmptyParameters}</li>
+	 * <li>Otherwise {@link HttpParametersMap}.
+	 * </ol>
+	 */
+	public static HttpParameters of(String queryString, String documentEncoding) throws UnsupportedEncodingException {
+		if(queryString == null || queryString.isEmpty()) {
+			return EmptyParameters.getInstance();
+		} else {
+			return new HttpParametersMap(queryString, documentEncoding);
+		}
 	}
 
 	/**
