c754f61b6a88db358f8158f1795771e0d8b4b1c3
AO Industries, Inc. <support@aoindustries.com>
2020-02-27 14:34:21 -0600

    com.aoindustries.util.logging.* significantly reworked
    to be compatible with logging.properties.

--- src/main/java/com/aoindustries/util/logging/ErrorPrinterFormatter.java
+++ src/main/java/com/aoindustries/util/logging/ErrorPrinterFormatter.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2009, 2010, 2011, 2013, 2016, 2019  AO Industries, Inc.
+ * Copyright (C) 2009, 2010, 2011, 2013, 2016, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -37,32 +37,30 @@ import java.util.logging.LogRecord;
  */
 public class ErrorPrinterFormatter extends Formatter {
 
-	private static final ErrorPrinterFormatter errorPrinterFormatter = new ErrorPrinterFormatter();
-	public static ErrorPrinterFormatter getInstance() {
-		return errorPrinterFormatter;
-	}
-
-	private ErrorPrinterFormatter() {
+	/**
+	 * Public constructor required so can be specified in <code>logging.properties</code>.
+	 */
+	public ErrorPrinterFormatter() {
 	}
 
 	public void format(LogRecord record, Appendable out) {
-		List<Object> extraInfo = new ArrayList<>(9); // At most 9 elements added below
+		List<Object> extraInfo = new ArrayList<>();
 		String loggerName = record.getLoggerName();
-		if(loggerName!=null) extraInfo.add("record.loggerName="+loggerName);
-		extraInfo.add("record.level="+record.getLevel());
-		extraInfo.add("record.sequenceNumber="+record.getSequenceNumber());
+		if(loggerName != null) extraInfo.add("record.loggerName=" + loggerName);
+		extraInfo.add("record.level=" + record.getLevel());
+		extraInfo.add("record.sequenceNumber=" + record.getSequenceNumber());
 		String sourceClassName = record.getSourceClassName();
-		if(sourceClassName!=null) extraInfo.add("record.sourceClassName="+sourceClassName);
+		if(sourceClassName != null) extraInfo.add("record.sourceClassName=" + sourceClassName);
 		String sourceMethodName = record.getSourceMethodName();
-		if(sourceMethodName!=null) extraInfo.add("record.sourceMethodName="+sourceMethodName);
+		if(sourceMethodName != null) extraInfo.add("record.sourceMethodName=" + sourceMethodName);
 		String message = record.getMessage();
-		if(message==null) message = "";
-		extraInfo.add("record.message="+message);
+		if(message == null) message = "";
+		extraInfo.add("record.message=" + message);
 		String formatted = formatMessage(record);
-		if(formatted==null) formatted = "";
-		if(!message.equals(formatted)) extraInfo.add("record.message.formatted="+formatted);
-		extraInfo.add("record.threadID="+record.getThreadID());
-		extraInfo.add("record.millis="+record.getMillis());
+		if(formatted == null) formatted = "";
+		if(!message.equals(formatted)) extraInfo.add("record.message.formatted=" + formatted);
+		extraInfo.add("record.threadID=" + record.getThreadID());
+		extraInfo.add("record.millis=" + record.getMillis());
 		Throwable thrown = record.getThrown();
 		ErrorPrinter.printStackTraces(
 			thrown,
---
+++ src/main/java/com/aoindustries/util/logging/HandlerUtil.java
@@ -0,0 +1,70 @@
+/*
+ * aocode-public - Reusable Java library of general tools with minimal external dependencies.
+ * Copyright (C) 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aocode-public.
+ *
+ * aocode-public is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aocode-public is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aocode-public.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.util.logging;
+
+import java.io.UnsupportedEncodingException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * Provides configuration method similar to {@link ConsoleHandler}.
+ * 
+ * @see ConsoleHandler
+ * 
+ * @author  AO Industries, Inc.
+ */
+public class HandlerUtil {
+
+	/**
+	 * Private method to configure a ConsoleHandler from LogManager
+	 * properties and/or default values as specified in the class
+	 * javadoc.
+	 *
+	 * @see ConsoleHandler#configure()
+	 */
+	public static void configure(Handler handler) {
+		LogManager manager = LogManager.getLogManager();
+		String cname = handler.getClass().getName();
+
+		handler.setLevel(LogManagerUtil.getLevelProperty(manager, cname +".level", Level.INFO));
+		handler.setFilter(LogManagerUtil.getFilterProperty(manager, cname +".filter", null));
+		handler.setFormatter(LogManagerUtil.getFormatterProperty(manager, cname +".formatter", new SimpleFormatter()));
+		try {
+			handler.setEncoding(LogManagerUtil.getStringProperty(manager, cname +".encoding", null));
+		} catch (RuntimeException | UnsupportedEncodingException ex) {
+			LogManagerUtil.warn(ex);
+			try {
+				handler.setEncoding(null);
+			} catch (RuntimeException | UnsupportedEncodingException ex2) {
+				LogManagerUtil.warn(ex2);
+				// doing a setEncoding with null should always work.
+				// assert false;
+			}
+		}
+	}
+
+	private HandlerUtil() {}
+}
