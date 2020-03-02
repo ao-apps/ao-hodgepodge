827eeaa8289ec1ccbc6e5f2e5df658afc073328d
AO Industries, Inc. <support@aoindustries.com>
2020-03-01 21:57:30 -0600

    TopologicalSorter now returns a modifiable Set.
    Previously it returned an unmodifiable List.

--- src/main/java/com/aoindustries/util/graph/GraphSorter.java
+++ src/main/java/com/aoindustries/util/graph/GraphSorter.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2011, 2016, 2019  AO Industries, Inc.
+ * Copyright (C) 2011, 2016, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,15 +22,17 @@
  */
 package com.aoindustries.util.graph;
 
-import java.util.List;
+import java.util.Set;
 
 /**
- * Sorts the vertices a graph into particular order.
+ * Sorts the vertices of a graph into a particular order.
  *
  * @author  AO Industries, Inc.
  */
 @FunctionalInterface
 public interface GraphSorter<V,EX extends Exception> {
 
-	List<V> sortGraph() throws EX;
+	// TODO: This should be given the graph as a parameter?
+	Set<V> sortGraph() throws EX;
 }
+
