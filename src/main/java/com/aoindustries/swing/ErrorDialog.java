80e84f7b139f94420c41becff8c79832d2337bb0
AO Industries, Inc <support@aoindustries.com>
2020-03-25 01:51:23 -0500

    Using varargs for exception "extraInfo"

--- src/main/java/com/aoindustries/swing/ErrorDialog.java
+++ src/main/java/com/aoindustries/swing/ErrorDialog.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public - Reusable Java library of general tools with minimal external dependencies.
- * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2016  AO Industries, Inc.
+ * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2016, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -48,10 +48,10 @@ public class ErrorDialog extends DefaultJDialog implements ActionListener {
 	private EnterJButton closeButton;
 
 	public ErrorDialog(Component parent, String title, Throwable T) {
-		this(parent, title, T, null);
+		this(parent, title, T, (Object[])null);
 	}
 
-	public ErrorDialog(Component parent, String title, Throwable T, Object[] extraInfo) {
+	public ErrorDialog(Component parent, String title, Throwable T, Object... extraInfo) {
 		super((parent instanceof JFrame) ? (JFrame)parent : new JFrame(), title, true, 400, 300);
 
 		Container contentPane=getContentPane();
